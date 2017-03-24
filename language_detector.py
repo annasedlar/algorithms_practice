80f90355ef5c595dc1fd08d9620f279488a4a951
Anna Sedlar <annasedlar@gmail.com>
2017-03-23 22:36:09 -0400

    language_detector algo ---did not bring in languages.py

---
+++ language_detector.py
@@ -0,0 +1,18 @@
+
+def detect_language(text, languages):
+    """Returns the detected language of given text."""
+    word_count = {}
+    
+    list_of_words = text.split()
+    
+    for item in languages:
+        counter = 0
+        for common_word in item['common_words']:
+            if common_word in list_of_words:
+                counter += 1
+        word_count[item['name']]=counter
+
+    maximum = max(word_count, key=word_count.get)
+    
+    return maximum
+    
