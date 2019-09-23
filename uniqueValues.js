67d3440536e74581a13d1cd8386d009b5907d513
Anna Sedlar <annasedlar@gmail.com>
2019-09-23 16:36:31 -0400

    return unique values from array of duplicates

---
+++ uniqueValues.js
@@ -0,0 +1,13 @@
+// Create a function that accepts an array parameter with duplicate values and returns a new array without duplicates.
+
+function filterArray(arr) {
+	let uniques = [];
+	arr.map(value => {
+		if (! uniques.includes(value) ) {
+			uniques.push(value)
+		}
+  })
+  console.log(uniques)
+}
+
+filterArray([1,1,2,2,2,2,2,2,2,3,4,5,5,6,2,7,9,10,4,5]);
